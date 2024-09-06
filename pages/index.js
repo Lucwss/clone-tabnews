cadfede746301cb0693436fb7a07669670e76c82
lucas <lucws2002@gmail.com>
2024-09-05 20:58:49 -0400

    style: 💄 add file `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function home() {
-  return <h1>Não pare, as vezes a sua benção pode estar mais perto do que imagina</h1>
+  return (
+    <h1>
+      Não pare, as vezes a sua benção pode estar mais perto do que imagina
+    </h1>
+  );
 }
 
-export default home
+export default home;
