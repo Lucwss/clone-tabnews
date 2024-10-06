ead5e4244c4ebc29a654dd0f2a79744470ca854c
lucas emanoel <lucws2002@gmail.com>
2024-10-05 20:07:55 -0400

    feat: finish status endpoint /api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,31 @@
-function status(request, response) {
+const { query } = require('../../../../infra/database.js')
+
+async function status(request, response) {
+  const updatedAt = new Date().toISOString()
+
+  const databaseName = process.env.POSTGRE_DB
+  const resultServerVersion = await query("SHOW server_version;")
+  const resultMaxConnections = await query("SHOW max_connections;")
+  const resultHealthy = await query("SELECT * FROM pg_stat_replication;")
+  const resultOpenedConnections = await query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  })
+
+  const serverVersion = resultServerVersion.rows[0].server_version
+  const maxConnections = resultMaxConnections.rows[0].max_connections
+  const healthy = resultMaxConnections.rows[0].max_connections
+  const openedConnections = resultOpenedConnections.rowCount
+
   return response.status(200).json({
-    message: "Testing tab news status não"
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: serverVersion,
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnections,
+      }
+    }
   })
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,12 +1,33 @@
-const { query } = require('../../../../../infra/database.js')
-
 test('GET to /api/v1/status should return 200', async () => {
 
-  const result = await query("SELECT 1 + 1 as sum;")
+  const response = await fetch('http://0.0.0.0:3000/api/v1/status')
+  expect(response.status).toBe(200)
 
-  console.log(result.rows)
+  const responseBody = await response.json()
+  expect(responseBody).toBeDefined()
+
+  const updatedAt = responseBody.updated_at
+  const dateParsedUpdatedAt = new Date(updatedAt).toISOString()
+  expect(updatedAt).toEqual(dateParsedUpdatedAt)
+
+  const dependencies = responseBody.dependencies
+  const database = dependencies.database
+
+  expect(database).toBeDefined()
+
+  const version = database.version
+  const status = database.status
+  const maxConnections = database.max_connections
+  const openedConnections = database.opened_connections
+
+  expect(version).toBeDefined()
+  expect(version).toEqual('16.4')
+
+  expect(maxConnections).toBeDefined()
+  expect(maxConnections).toEqual(100)
+
+  expect(openedConnections).toBeDefined()
+  expect(openedConnections).toEqual(1)
 
 
-  const { status } = await fetch('http://0.0.0.0:3000/api/v1/status')
-  expect(status).toBe(200)
 })
