b7a0196c61c1b3fd91e22a030a4659e4a976f4b3
lucas emanoel <lucws2002@gmail.com>
2024-10-05 20:07:04 -0400

    refactor: make database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -1,29 +1,26 @@
 const pg = require('pg')
 
-const client = new pg.Client({
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  user: process.env.POSTGRES_USER,
-  database: process.env.POSTGRE_DATABASE,
-  password: process.env.POSTGRES_PASSWORD
-})
-
-
-async function connect() {
-  await client.connect()
-  return client
-}
-
-async function closeConnection() {
-  await client.end()
-}
-
-
 async function query(queryObject) {
-  const connectedClient = await connect()
-  const result = await connectedClient.query(queryObject)
-  await closeConnection()
-  return result
+
+  const client = new pg.Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRE_DB,
+    password: process.env.POSTGRES_PASSWORD
+  })
+
+  await client.connect()
+
+  try {
+    const result = await client.query(queryObject)
+    return result
+  } catch (error) {
+    console.error(error)
+  }
+  finally {
+    await client.end()
+  }
 }
 
 
