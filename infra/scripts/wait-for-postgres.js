ab298a8c921963509d87c3283c8fe25d46ff8c77
lucas <lucws2002@gmail.com>
2025-02-01 20:18:17 -0400

    feat: add  script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,16 @@
+const { exec } = require('node:child_process')
+
+function checkPostgres() {
+  exec('docker exec postgres-dev pg_isready --host localhost', (error, stdout) => {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write('.');
+      setTimeout(checkPostgres, 1000);
+      return
+    }
+
+    console.log('\n🟢 PostgreSQL is ready!\n');
+  })
+}
+
+console.log('\n\n🔴 Waiting for PostgreSQL to become available');
+checkPostgres();
