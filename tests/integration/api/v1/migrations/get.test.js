b40526d7b7f5acf8fbba9f0b42ba9ca11749007e
lucas <lucws2002@gmail.com>
2025-09-13 17:49:48 -0400

    refactor: change test name from 'running' to 'retrieving' migrations

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -14,7 +14,7 @@ async function cleanDatabase() {
 
 describe("GET /api/v1/migrations", () => {
   describe("Anonymous user", () => {
-    test("Running pending migations", async () => {
+    test("Retrieving pending migations", async () => {
       const response = await fetch("http://0.0.0.0:3000/api/v1/migrations");
       expect(response.status).toBe(200);
 
