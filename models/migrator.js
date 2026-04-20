deab165b6a1a8fd5b9b153068e976d007175499b
Lucas Emanoel <lucws2002@gmail.com>
2026-04-20 12:36:23 -0400

    feat: create migrator model

---
+++ models/migrator.js
@@ -0,0 +1,52 @@
+import { runner } from "node-pg-migrate";
+import { resolve } from "node:path";
+import database from "infra/database";
+
+const defaultMigrationOptions = {
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  dryRun: true,
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+    const pendingMigrations = await runner({
+      dbClient,
+      ...defaultMigrationOptions,
+    });
+
+    return pendingMigrations
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await runner({
+      dbClient,
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+
+    return migratedMigrations
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations
+}
+
+export default migrator;
