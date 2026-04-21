0920351fdfccc0ac9df3d5f49a7cd9a36e4d1410
Lucas Emanoel <lucws2002@gmail.com>
2026-04-21 17:40:23 -0400

    feat: add migration to create users table

---
+++ infra/migrations/1776804254182_create-users.js
@@ -0,0 +1,36 @@
+const { password } = require("pg/lib/defaults");
+
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()")
+    },
+    // for reference, Github limits usernames to 39 characters.
+    username: {
+      type: "varchar(30)",
+      notNull: true,
+      unique: true
+    },
+    email: {
+      type: "varchar(254)",
+      notNull: true,
+      unique: true
+    },
+    password: {
+      type: "varchar(72)",
+      notNull: true,
+    },
+    created_at: {
+      type: "timestamptz",
+      default: pgm.func("now()")
+    },
+    updated_at: {
+      type: "timestamptz",
+      default: pgm.func("now()"),
+    },
+  })
+};
+
+exports.down = false;
