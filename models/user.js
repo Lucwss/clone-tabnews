929c54a3d42cd945a29de293336c9f8c9c0348db
Lucas <lucws2002@gmail.com>
2026-06-29 00:35:57 -0400

    feat: hash password in user.create

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
 import database from "infra/database.js";
 import { ValidationError, NotFoundError } from "infra/errors.js";
+import password from "models/password.js";
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
   await validateUniqueUsername(userInputValues.username);
+  await hashPasswordInObject(userInputValues)
 
   const newUser = await runInQuery(userInputValues);
   return newUser;
@@ -50,6 +52,11 @@ async function create(userInputValues) {
         action: "Choose a different email.",
       });
     }
+  }
+
+  async function hashPasswordInObject(userInputValues) {
+    const hashedPassword = await password.hash(userInputValues.password);
+    userInputValues.password = hashedPassword;
   }
 
   async function runInQuery() {
